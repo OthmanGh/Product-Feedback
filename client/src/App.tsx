cb888637a7d8145d5b254cf19783d95fa6ea6094
OthmanGh <othmanghandour51@gmail.com>
2024-10-29 22:21:08 +0200

    add browser router and set home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, DefaultLayout } from './constants';
+
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<DefaultLayout />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
