27767ae59f75ed7250aacb410c02e0b1dd37072c
OthmanGh <othmanghandour51@gmail.com>
2024-10-29 22:19:41 +0200

    create mobile ui's for header & sidebars

---
+++ client/src/ui/MobileHeader.tsx
@@ -0,0 +1,34 @@
+type MobileHeaderProps = {
+  isModalOpen: boolean;
+  toggleMobileModal: () => void;
+};
+
+const MobileHeader = ({
+  isModalOpen,
+  toggleMobileModal,
+}: MobileHeaderProps) => {
+  return (
+    <header className='bg-tablet-header fill_bg flex_between px-10 py-4 text-white sm:hidden'>
+      <div className='flex flex-col'>
+        <h1 className='font-bold text-heading-3'>Frontend Mentor</h1>
+        <p className='text-body-2 opacity-75'>Feedback Board</p>
+      </div>
+
+      {isModalOpen ? (
+        <img
+          src='/shared/mobile/icon-close.svg'
+          onClick={toggleMobileModal}
+          className='toggle_modal'
+        />
+      ) : (
+        <img
+          src='/shared/mobile/icon-hamburger.svg'
+          className='toggle_modal'
+          onClick={toggleMobileModal}
+        />
+      )}
+    </header>
+  );
+};
+
+export default MobileHeader;
---
+++ client/src/ui/MobileSidebar.tsx
@@ -0,0 +1,13 @@
+import FeedbackNavItems from './FeedbackNavItems';
+import RoadmapCard from './RoadmapCard';
+
+const MobileSidebar = () => {
+  return (
+    <div className='absolute bg-blue-light top-13 right-0 left-[22%] h-full z-10 flex flex-col gap-6 items-center sm:hidden p-5'>
+      <FeedbackNavItems />
+      <RoadmapCard />
+    </div>
+  );
+};
+
+export default MobileSidebar;
