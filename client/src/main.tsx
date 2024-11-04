d591b9d28a47a61668b93ec6c1c0381326cdcbc4
OthmanGh <othmanghandour51@gmail.com>
2024-11-04 12:12:26 +0200

    feat: wrap app with context providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { FeedbackStorageProvider } from './context/FeedbackStorageContext.tsx';
+import { FeedbackQueryProvider } from './context/FeedbackQueryContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <FeedbackStorageProvider>
+    <FeedbackQueryProvider>
+      <App />
+    </FeedbackQueryProvider>
+  </FeedbackStorageProvider>
+);
