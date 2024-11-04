c4c7f67fb13f0e262ea75c22f9b610dea849ed55
OthmanGh <othmanghandour51@gmail.com>
2024-11-04 12:07:10 +0200

    feat: modify custom button component

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,14 +1,49 @@
 type ButtonProps = {
   label: string;
-  color: string;
+  color: 'blue' | 'purple' | 'gray' | 'red';
   onClick: () => void;
+  className?: string;
+  type?: 'button' | 'reset' | 'submit';
 };
 
-const Button = ({ label, color, onClick }: ButtonProps) => {
+const Button = ({
+  label,
+  color,
+  onClick,
+  className,
+  type = 'button',
+}: ButtonProps) => {
+  const actualColor =
+    color === 'purple'
+      ? '#AD1FEA'
+      : color === 'blue'
+      ? '#4661E6'
+      : color === 'gray'
+      ? '#3A4374'
+      : '#D73737';
+
+  const hoverColor =
+    color === 'purple'
+      ? '#C75AF6'
+      : color === 'blue'
+      ? '#7C91F9'
+      : color === 'gray'
+      ? '#656EA3'
+      : '#E98888';
+
   return (
     <button
-      className={`bg-${color} py-3 px-4 rounded-[10px] text-white font-semibold`}
+      style={{
+        backgroundColor: actualColor,
+        transition: 'background-color 0.3s',
+      }}
+      className={`py-3 px-4 lg:py-[12px] lg:px-6 rounded-[10px] text-white font-semibold ${className}`}
       onClick={onClick}
+      type={type}
+      onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = hoverColor)}
+      onMouseLeave={(e) =>
+        (e.currentTarget.style.backgroundColor = actualColor)
+      }
     >
       {label}
     </button>
