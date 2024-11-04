5051f6ed4200dd28559d3bb9af168715b4071b8a
OthmanGh <othmanghandour51@gmail.com>
2024-11-04 12:11:23 +0200

    feat: update imports/exports file

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -6,13 +6,38 @@ import MobileSidebar from '../ui/MobileSidebar';
 import FeedbackNavItems from '../ui/FeedbackNavItems';
 import RoadmapCard from '../ui/RoadmapCard';
 import Button from '../components/Button';
-import { FaCheck } from 'react-icons/fa';
 import FilterBar from '../ui/FilterBar';
 import CustomDropdown from '../components/CustomDropdown';
+import ProductFeedbackContainer from '../ui/ProductFeedbackContainer';
+import CategoryTag from '../ui/mini/CategoryTag';
+import Comments from '../ui/mini/Comments';
+import Upvotes from '../ui/mini/Upvotes';
+import FeedbackItem from '../ui/FeedbackItem';
+import EmptyProductFeedback from '../ui/EmptyProductFeedback';
+import MainBoard from '../ui/MainBoard';
+import FeedbackBoardCard from '../ui/FeedbackBoardCard';
+import CreateNewFeedback from '../pages/CreateNewFeedback';
+import BackButton from '../ui/BackButton';
+import Roadmap from '../pages/Roadmap';
+import RoadmapHeader from '../ui/RoadmapHeader';
+import RoadmapMobileNavbar from '../ui/RoadmapMobileNavbar';
+import RoadmapCardsMobile from '../ui/RoadmapCardsMobile';
+import RoadmapCards from '../ui/RoadmapCards';
 
 //* icons
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { MdKeyboardArrowUp } from 'react-icons/md';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+
+import { FaCheck } from 'react-icons/fa';
+
+// dummy
+import { dummyData } from '../context/dummy';
+const { user, feedbackRequests } = dummyData;
+
+// utils:
+import { sortFeedbackRequests } from '../utils/helpers';
 
 export {
   MobileHeader,
@@ -28,6 +53,26 @@ export {
   MdKeyboardArrowDown,
   FaCheck,
   FilterBar,
+  ProductFeedbackContainer,
+  user,
+  feedbackRequests,
+  FeedbackItem,
+  Upvotes,
+  Comments,
+  CategoryTag,
+  EmptyProductFeedback,
+  BackButton,
+  MainBoard,
+  FeedbackBoardCard,
+  CreateNewFeedback,
+  MdKeyboardArrowRight,
+  MdKeyboardArrowLeft,
+  Roadmap,
+  RoadmapHeader,
+  RoadmapMobileNavbar,
+  RoadmapCardsMobile,
+  RoadmapCards,
+  sortFeedbackRequests,
 };
 
 export const menuNavItems = [
@@ -103,5 +148,23 @@ export const selectOptions = [
   {
     label: 'Least Comments',
     value: 'least-comments',
+  },
+];
+
+export const MobileRoadmapNavItems = [
+  {
+    label: 'Planned',
+    value: 'planned',
+    color: '#F49F85',
+  },
+  {
+    label: 'In Progress',
+    value: 'in-progress',
+    color: '#AD1FEA',
+  },
+  {
+    label: 'Live',
+    value: 'live',
+    color: '#62BCFA',
   },
 ];
