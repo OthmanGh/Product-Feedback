723f49ef937472aca8635c1654f83e9985615fd0
Osman <othmanghandour51@gmail.com>
2024-12-28 00:25:27 +0200

    Frontend: add guests layout & not found routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, DefaultLayout, CreateNewFeedback, Roadmap } from './constants';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {
+  Home,
+  DefaultLayout,
+  CreateNewFeedback,
+  Roadmap,
+  GuestsLayout,
+  Login,
+  Signup,
+  PageNotFound,
+} from "./constants"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<DefaultLayout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/create-new-feedback' element={<CreateNewFeedback />} />
-          <Route path='/roadmap' element={<Roadmap />} />
+          <Route path='' element={<Home />} />
+          <Route path='create-new-feedback' element={<CreateNewFeedback />} />
+          <Route path='roadmap' element={<Roadmap />} />
         </Route>
+
+        <Route path='/auth' element={<GuestsLayout />}>
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
+        </Route>
+
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
